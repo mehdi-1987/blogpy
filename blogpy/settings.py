bd0bfe4a9886ea7029e1d20b33d37dea05dccd28
mehdi-1987 <mehdi_sh_1987@yahoo.com>
2020-10-15 12:27:14 +0330

    add index.html to blog app

--- blogpy/settings.py
+++ blogpy/settings.py
@@ -131,9 +131,12 @@ USE_TZ = True
 
 #CKeditor Settings
 #Static Files
-STATIC_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
 
+)
 #CKeditor ckeditor_uploader
 
 CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
